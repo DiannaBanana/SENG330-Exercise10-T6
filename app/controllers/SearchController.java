ff859c1733d7526551fd28c50a3ed67903dc08a2
Yiliang Liu <tony1997810@gmail.com>
2020-12-11 00:47:54 -0800

    Merge species and date search

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -11,11 +11,17 @@ import play.mvc.Http;
 import play.mvc.Result;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Locale;
+
 
 public class SearchController extends Controller {
 
@@ -35,48 +41,32 @@ public class SearchController extends Controller {
         activeModel = model;
     }
 
-    public Result searchObservation(Http.Request r, Long obsId) {
-        Optional<Observation> observation = activeModel.getObservationStore().getObservationById(obsId);
-
-        if (observation.isPresent()) {
-            return ok(views.html.observationDetail.render(observation.get(), whaleDataForm, r, me.preferred(r)));
-        }
-
-        return redirect(routes.Driver.index());
-    }
-
-    public Result searchObservationBySpecies(Http.Request r, String species) {
+    public Result searchObservation(Http.Request r, String keyword) {
         int obsId = 0;
         List<Observation> obs= activeModel.getObservationStore().getObservations();
+        if(keyword.matches("\\d{4}-\\d{2}-\\d{2}")){
 
-        for(int i = 0; i < obs.size(); i++){
-            Set<Whale> whales = obs.get(i).getWhales();
-            List<Whale> whale_list = new ArrayList<>();
-            whale_list.addAll(whales);
-            for(int j = 0; j < whale_list.size(); j++){
-                if(whale_list.get(j).getSpecies().equals("orca")){
-                    obsId = i;
+            LocalDate date = LocalDate.parse(keyword);
+            System.out.println("date string : " + keyword +", " + "localdate : " + date);
+            for(int k = 0; k < obs.size(); k++){
+                if(obs.get(k).getTime().toLocalDate().isEqual(date)){
+                    System.out.println(obs.get(k).getTime().toLocalDate());
+                    obsId=k;
+                }
+            }
+        }else{
+            for(int i = 0; i < obs.size(); i++){
+                Set<Whale> whales = obs.get(i).getWhales();
+                List<Whale> whale_list = new ArrayList<>();
+                whale_list.addAll(whales);
+                for(int j = 0; j < whale_list.size(); j++){
+                    if(whale_list.get(j).getSpecies().equals(keyword)){
+                        obsId = i;
+                    }
                 }
             }
         }
-        Optional<Observation> observation = activeModel.getObservationStore().getObservationById(obsId);
 
-        if (observation.isPresent()) {
-            return ok(views.html.observationDetail.render(observation.get(), whaleDataForm, r, me.preferred(r)));
-        }
-
-        return redirect(routes.Driver.index());
-    }
-
-    public Result searchObservationByTime(Http.Request r, LocalDateTime time) {
-        int obsId = 0;
-        List<Observation> obs= activeModel.getObservationStore().getObservations();
-
-        for(int i = 0; i < obs.size(); i++){
-            if(obs.get(i).getTime().isEqual(time)){
-
-            }
-        }
         Optional<Observation> observation = activeModel.getObservationStore().getObservationById(obsId);
 
         if (observation.isPresent()) {
