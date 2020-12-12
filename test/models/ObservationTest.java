2030fc4418a511bb0890573206afc5b5686175d6
a-krawciw <freakyfactoid@gmail.com>
2020-12-11 17:38:29 -0800

    Shrank image files for better web use

--- test/models/ObservationTest.java
+++ test/models/ObservationTest.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class ObservationTest {
+
 
     @Test
     public void testThrowsOnNull(){
@@ -19,7 +21,7 @@ public class ObservationTest {
     @Test
     public void idIncrements(){
         Observation o = null;
-        o.setObservationCount(0);
+        Observation.setObservationCount(0);
         for (int i = 0; i < 5; i++) {
             o = new Observation(LocalDateTime.now(), "Hawaii");
         }
@@ -35,16 +37,4 @@ public class ObservationTest {
 
         assertEquals(1, o.getWhales().size());
     }
-
-    @Test
-    public void addMultipleWhales(){
-        Whale w = new Whale("fin", 150, "f");
-        Whale x = new Whale("beluga", 300, "m");
-        Observation obs = new ObservationBuilder().observedAt(LocalDateTime.now()).atLocation("here").build();
-        obs.getWhales().add(w);
-        obs.getWhales().add(x);
-
-        assertEquals(2, obs.getWhales().size());
-    }
-
 }
